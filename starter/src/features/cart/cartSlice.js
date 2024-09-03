2f748e78d98f76a6cc30d9ff289ae93089394694
Nirmal Gusain <155016618+Nirmal-Gusain@users.noreply.github.com>
2024-09-03 20:48:06 +0530

    Updated

--- starter/src/features/cart/cartSlice.js
+++ starter/src/features/cart/cartSlice.js
@@ -18,6 +18,14 @@ const cartSlice = createSlice({
         removeItem:(state,action)=>{
           const ItemId  = action.payload;
           state.cartItems = state.cartItems.filter((item)=>item.id !== ItemId)
+        },
+        increase:(state,{payload})=>{
+          const cartItems = state.cartItems.find((item)=>item.id === payload.id)
+          cartItems.amount = cartItems.amount + 1
+        },
+        decrease:(state,{payload})=>{
+          const cartItems = state.cartItems.find((item)=>item.id === payload.id)
+          cartItems.amount = cartItems.amount - 1
         }
       },
     
@@ -25,4 +33,4 @@ const cartSlice = createSlice({
 
 
 export default cartSlice.reducer;
-export const {clearCart,removeItem} = cartSlice.actions;
+export const {clearCart,removeItem,increase,decrease} = cartSlice.actions;
